20e9bdf80dafd4bb6c57cc3569e39fc828ad2dd8
Wesleson Souza Silva <31991735+weslleson22@users.noreply.github.com>
2020-10-20 06:29:17 -0300

    Update projeto2.py

--- projeto2.py
+++ projeto2.py
@@ -1,33 +1,76 @@
-
+import random
+import time
+from datetime import datetime
+import csv
+start_time = datetime.now()
 
 
 f = open("file.txt",'r')
-linha = f.readlines()
+linha = int(f.readline())
 
-x = 0
+lista1=[]
 
-while x < len(linha):
-    if linha[x] == "\n":
-        local = linha.index(linha[x])
-        linha.pop(local)
-    else:
-        linha[x] = linha[x].split(',')
-        x += 1
-
-# Esse for abaixo aqui é só para tirar o "\n" em algumas strings, é opcional.
-
-for i in linha:
-    local = linha.index(i) # Local do i em texto
-    for b in i:
-        local2 = linha[local].index(b) # Local2 do b em i ( local )
-        if "\n" in b:
-            linha[local][local2] = b.replace("\n",'') # Substitui o valor de acordo com "local" e "local2"
-
-lista1, lista2 = linha
-print("linha1 =",lista1)
-print("linha2 =",lista2)
+for i in range (linha):
+    lista1.append(random.randint(0,linha))
 
 
-#soma=lista1+lista1
-#print(soma)
+def bubbleSort(alist):
+    for passnum in range(len(alist)-1,0,-1):
+        for i in range(passnum):
+            if alist[i]>alist[i+1]:
+                #irá trocar os itens i e j da lista. Sem o armazenamento temporário, um dos valores seria sobrescrito.
+                temp = alist[i]
+                alist[i] = alist[i+1]
+                alist[i+1] = temp
 
+#bubbleSort(lista1)
+#print(lista1)
+
+#Selection Sort 
+for i in range(len(lista1)): 
+      
+    # Find the minimum element in remaining  
+    # unsorted array 
+    min_idx = i 
+    for j in range(i+1, len(lista1)): 
+        if lista1[min_idx] > lista1[j]: 
+            min_idx = j 
+              
+    # Swap the found minimum element with  
+    # the first element         
+    lista1[i], lista1[min_idx] = lista1[min_idx], lista1[i] 
+  
+# Driver code to test above 
+
+#print([i]),  
+for i in range(len(lista1)): 
+    print(lista1[i]) 
+
+    def insertionsort(lista1):
+        # começamos o loop no segundo elemento (índice 1), uma vez que o primeiro item já está classificado
+        for j in range(1,len(lista1)):
+            key = lista1[j] #O próximo item que vamos inserir na seção classificada da matriz
+
+            i = j-1 # o último item com o qual iremos comparar
+            #agora continuamos movendo a chave para trás, desde que seja menor do que o último item da matriz
+            while (i > -1) and key < lista1[i]: #if i == -1 significa que esta chave pertence ao início
+                lista1[i+1]=lista1[i] #move o último objeto comparado uma etapa à frente para abrir espaço para a chave
+                i=i-1#observe o próximo item para a próxima vez.
+            #okay i não é maior que key significa que a key pertence a i + 1
+            lista1[i+1] = key
+
+       # print(lista1) 
+        return lista1
+
+
+insertionsort(lista1)
+
+
+tempo=end_time = datetime.now()
+Duracao=format(end_time - start_time)
+print('Duração de execução:',Duracao)
+print(tempo)
+
+c = open("result.csv", "wb")
+c.write('a')
+c.close()
